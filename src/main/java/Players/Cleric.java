28053eb621021bec790fbaf5fd08baf8daa8d947
Raphael Ugha <raphaelugha@gmail.com>
2018-08-09 15:21:28 +0100

    Created Cleric, test passed

---
+++ src/main/java/Players/Cleric.java
@@ -0,0 +1,24 @@
+package Players;
+
+import Interfaces.IHeal;
+
+public class Cleric extends Player {
+    private IHeal medicine;
+
+    public Cleric(String name, IHeal medicine){
+        super(name);
+        this.medicine = medicine;
+        this.HP = 150;
+    }
+
+    public IHeal getMedicine(){
+        return medicine;
+    }
+    public void setMedicine(IHeal medicine){
+        this.medicine = medicine;
+    }
+
+    public void heal(Player player){
+        this.medicine.heal(player);
+    }
+}
